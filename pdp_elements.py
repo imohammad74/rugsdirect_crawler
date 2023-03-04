fec33c9efd7ea09f22591b457b293da228367173
imohammad <m.razmara.1374@gmail.com>
2023-03-05 02:51:38 +0330

    fix title, desc, feature

--- pdp_elements.py
+++ pdp_elements.py
@@ -20,86 +20,42 @@ class PDPElements:
     @staticmethod
     def title(soup):
         """get title of pdp"""
-        title = soup.find(class_='ProductDetailsProductName no-m-t').text
+        title = soup.find(class_='pdp-title').text
+        print(title)
         return title
 
     @staticmethod
     def description(soup):
-        desc = soup.find(class_='desc col-sm-7').text
+        desc = soup.find(class_='product-overview-data').text
+        print (desc)
         return desc
 
-    @staticmethod
-    def feature_section_type_1(content):
-        a = str(content).split('<br/>')
-        features_label = [i.split(':')[0].replace(' ', '_') for i in a if i.split(':')[0][0] != '<']
-        features = []
-        for i in a:
-            fe = i.split('href')
-            fe_part = len(fe)
-            if fe_part == 1:
-                if ':' in fe[0]:
-                    fe = fe[0].split(':')
-                    feature = fe[1]
-                    if '<sup>' and '</sup>' and '<sub>' and '</sub>' in feature:
-                        feature = Common.convert_sup_sub_to_str(feature)
-                else:
-                    continue
-                features.append(feature.strip())
-            elif fe_part == 2:
-                m = BeautifulSoup(str(i), 'html.parser')
-                feature = m.find('a').text
-                if feature != 'Rug pad':
-                    features.append(feature.strip())
-            else:
-                continue
-        z = {}
-        for i in range(0, len(features)):
-            z['%s' % features_label[i]] = features[i]
-        return z
 
     @staticmethod
-    def feature_section_type_2(soup):
-        section = soup.find(class_='bullets col-sm-5')
-        a = str(section).split('<br/>')
-        del a[0]
-        del a[-1]
-        features_label = [i.split(':')[0].replace(' ', '_') for i in a if not re.search('<a', i.split(':')[0])]
-        if '\n' or '' in features_label:
-            features_label.remove('\n')
-            features_label.remove('')
-        features = []
-        for i in a:
-            fe = i.split('href')
-            fe_part = len(fe)
-            if fe_part == 1:
-                if ':' in fe[0]:
-                    fe = fe[0].split(':')
-                    feature = fe[1]
-                    if '<sup>' and '</sup>' and '<sub>' and '</sub>' in feature:
-                        feature = Common.convert_sup_sub_to_str(feature)
-                else:
-                    continue
-                features.append(feature.strip())
-            elif fe_part == 2:
-                m = BeautifulSoup(str(i), 'html.parser')
-                feature = m.find('a').text
-                if feature != 'Rug pad':
-                    features.append(feature.strip())
-            else:
-                continue
-        z = {}
-        for i in range(0, len(features)):
-            z['%s' % features_label[i]] = features[i]
-        return z
+    def features_title(soup):
+        content = soup.find_all(class_='rug-features-left')
+        titles = [title.text for title in content]
+        print(titles)
+        return titles
+
+    @staticmethod
+    def feature_value(soup):
+        content = soup.find_all(class_='rug-features-right')
+        values = [value.text for value in content]
+        print(values)
+        return values
 
     def features(self, soup):
-        content = soup.find(class_='bullets col-sm-5').find('span')
-        if content is not None:
-            features = self.feature_section_type_1(content)
-            return features
-        else:
-            features = self.feature_section_type_2(soup)
-            return features
+        features = []
+        titles = self.features_title(soup)
+        values = self.feature_value(soup)
+        for i in range(len(titles)):
+            feature = {}
+            feature['title'] = titles[i]
+            feature['value'] = values[i]
+            features.append(feature)
+        print(features)
+        return features
 
     @staticmethod
     def images_product(url: str, soup: str, download_image: bool):
