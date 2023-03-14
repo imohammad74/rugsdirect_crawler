91eb0a81b7384fd9304f98a83c1c5a90c06c30f8
imohammad <m.razmara.1374@gmail.com>
2023-03-14 17:24:28 +0330

    refactor for rugsdirect

--- pdp.py
+++ pdp.py
@@ -3,63 +3,52 @@ from bs4 import BeautifulSoup
 
 from db import DBManagement as db
 from pdp_elements import PDPElements
-from table import Table
+from table import PriceTable
 
 
 class PDP:
-
     @staticmethod
     def main(url: str):
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
-        if PDPElements.page_is_exist(soup):
-            features = list(PDPElements().features(soup).keys())
-            variants = Table().body(url, soup)
-            pattern_id = db.fetch_datas(db_file=db.db_file(), table_name=db.db_table()[1], all_columns=False,
-                                        columns=['brand_name', 'design_id_pattern'])
-            for variant in variants:
-                title = PDPElements.title(soup)
-                description = PDPElements.description(soup)
-                design_id = PDPElements().design_id(soup, pattern_id)
-                item_id = variants[variants.index(variant)]['Item #']
-                size = variant['Size']
-                ships_within = variant['Ships Within']
-                msrp = variant['MSRP']
-                sale_price = variant['Sale Price']
-                # image_urls = PDPElements.images_product(url, soup, download_image=False)
-                all_columns = [
-                    {'column': 'title', 'value': title},
-                    {'column': 'description', 'value': description},
-                    {'column': 'url', 'value': url},
-                    {'column': 'size', 'value': size},
-                    {'column': 'ships_within', 'value': ships_within},
-                    {'column': 'msrp', 'value': msrp},
-                    {'column': 'design_id', 'value': design_id},
-                    {'column': 'sale_price', 'value': sale_price}
-                ]
+        features = list(PDPElements().features(soup).keys())
+        feature_values = PDPElements.feature_value(soup)
+        variants = PriceTable.main(soup)
+        for variant in variants:
+            title = PDPElements.title(soup)
+            description = PDPElements.description(soup)
+            design_id = PDPElements().design_id(soup)
+            size = PDPElements.shape_size(soup)[variants.index(variant)][0]
+            msrp = variant['msrp']
+            sale_price = variant['price']
+            # image_urls = PDPElements.images_product(url, soup, download_image=False)
+            all_columns = [
+                {'column': 'title', 'value': title},
+                {'column': 'description', 'value': description},
+                {'column': 'url', 'value': url},
+                {'column': 'size', 'value': size},
+                {'column': 'msrp', 'value': msrp},
+                {'column': 'design_id', 'value': design_id},
+                {'column': 'sale_price', 'value': sale_price}
+            ]
+
+            for feature in features:
+                feature_value = feature_values[features.index(feature)]
                 try:
-                    db.insert_rows(db_file=db.db_file(), table_name=db.db_table()[2],
-                                   columns=[{'column': 'item_id', 'value': item_id}])
+                    db.update_rows(db_file=db.db_file(), table_name=db.db_table()[2],
+                                   condition=f'design_id="{design_id}"',
+                                   columns=[{'column': f'{feature}', 'value': feature_value}])
                 except:
-                    db.insert_rows(db_file=db.db_file(), table_name=db.db_table()[2],
-                                   columns=[{'column': 'item_id', 'value': ''}])
-                for feature in features:
-                    feature_value = PDPElements().features(soup)[f'{feature}']
-                    try:
-                        db.update_rows(db_file=db.db_file(), table_name=db.db_table()[2],
-                                       condition=f'item_id="{item_id}"',
-                                       columns=[{'column': f'{feature}', 'value': feature_value}])
-                    except:
-                        db.update_rows(db_file=db.db_file(), table_name=db.db_table()[2],
-                                       condition=f'item_id="{item_id}"',
-                                       columns=[{'column': f'{feature}', 'value': ''}])
-                try:
-                    db.update_rows(db_file=db.db_file(), table_name=db.db_table()[2], condition=f'item_id="{item_id}"',
-                                   columns=all_columns)
-                except:
-                    db.update_rows(db_file=db.db_file(), table_name=db.db_table()[2], condition=f'item_id="{item_id}"',
-                                   columns=all_columns)
-            print(f'"{title}" finish!')
+                    db.update_rows(db_file=db.db_file(), table_name=db.db_table()[2],
+                                   condition=f'design_id="{design_id}"',
+                                   columns=[{'column': f'{feature}', 'value': ''}])
+            try:
+                db.update_rows(db_file=db.db_file(), table_name=db.db_table()[2], condition=f'design_id="{design_id}"',
+                               columns=all_columns)
+            except:
+                db.update_rows(db_file=db.db_file(), table_name=db.db_table()[2], condition=f'design_id="{design_id}"',
+                               columns=all_columns)
+        print(f'"{title}" finish!')
         else:
             try:
                 db.insert_rows(db_file=db.db_file(), table_name=db.db_table()[3],
