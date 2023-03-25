ab4031d4e2496827bcf7f5d9150ee32ce6377174
imohammad <m.razmara.1374@gmail.com>
2023-03-25 09:48:30 +0330

    Handle out of stock product

--- pdp.py
+++ pdp.py
@@ -17,40 +17,45 @@ class PDP:
         brand = data[1]
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
-        features = PDPElements().features_title(soup)
-        feature_values = PDPElements.feature_value(soup)
-        variants = PriceTable.main(soup)
-        title = PDPElements.title(soup)
-        collection = PDPElements().collection(soup)
-        description = PDPElements.description(soup)
-        design_id = PDPElements().design_id(soup)
-        construction = feature_values[(features.index('Construction'))]
-        material = feature_values[(features.index('Material'))]
-        for variant in variants:
-            size = PDPElements.shape_size(soup)[variants.index(variant)][0]
-            msrp = variant['msrp']
-            sale_price = variant['price']
-            all_columns = [
-                {'column': 'title', 'value': title},
-                {'column': 'description', 'value': description},
-                {'column': 'url', 'value': url},
-                {'column': 'size', 'value': size},
-                {'column': 'brand', 'value': brand},
-                {'column': 'weave', 'value': construction},
-                {'column': 'material', 'value': material},
-                {'column': 'msrp', 'value': msrp},
-                {'column': 'collection', 'value': collection},
-                {'column': 'design_id', 'value': design_id},
-                {'column': 'sale_price', 'value': sale_price}
-            ]
-            try:
-                db.insert_rows(db_file=db.db_file(), table_name=db.db_table()[2], columns=all_columns)
-                # print(all_columns)
-            except:
-                db.insert_rows(db_file=db.db_file(), table_name=db.db_table()[3],
-                               columns=[{'column': 'url', 'value': url}])
-                # print('error')
-        print(f'"{title}" finish!')
+        if PDPElements.is_in_stock(soup):
+            features = PDPElements().features_title(soup)
+            feature_values = PDPElements.feature_value(soup)
+            variants = PriceTable.main(soup)
+            title = PDPElements.title(soup)
+            collection = PDPElements().collection(soup)
+            description = PDPElements.description(soup)
+            design_id = PDPElements().design_id(soup)
+            construction = feature_values[(features.index('Construction'))]
+            material = feature_values[(features.index('Material'))]
+            for variant in variants:
+                size = PDPElements.shape_size(soup)[variants.index(variant)][0]
+                msrp = variant['msrp']
+                sale_price = variant['price']
+                all_columns = [
+                    {'column': 'title', 'value': title},
+                    {'column': 'description', 'value': description},
+                    {'column': 'url', 'value': url},
+                    {'column': 'size', 'value': size},
+                    {'column': 'brand', 'value': brand},
+                    {'column': 'weave', 'value': construction},
+                    {'column': 'material', 'value': material},
+                    {'column': 'msrp', 'value': msrp},
+                    {'column': 'collection', 'value': collection},
+                    {'column': 'design_id', 'value': design_id},
+                    {'column': 'sale_price', 'value': sale_price}
+                ]
+                try:
+                    db.insert_rows(db_file=db.db_file(), table_name=db.db_table()[2], columns=all_columns)
+                    # print(all_columns)
+                except:
+                    db.insert_rows(db_file=db.db_file(), table_name=db.db_table()[3],
+                                   columns=[{'column': 'url', 'value': url}])
+                    # print('error')
+            print(f'"{title}" finish!')
+        else:
+            print('Product is out of stock')
+            query = f'''INSERT INTO NoData (URLAddress, ErrorMsg) VALUES ('{url}', 'Out of stock')'''
+            db.custom_query(db_file=db.db_file(), query=query)
 
     def __init__(self, data):
         # random_time = random.randint(2, 4)
